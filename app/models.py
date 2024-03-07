6f25bcb8bbd4476032f0a77f63bf9cd999780e7c
Rotimi Joseph <rotimi.joseph@hotmail.com>
2024-03-07 14:11:28 +0000

    add view set functionality, able to see all the flashcards

--- app/models.py
+++ app/models.py
@@ -23,3 +23,7 @@ class FlashCard(db.Model):
     question = db.Column(db.String(100), nullable=False)
     answer = db.Column(db.String(100), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    
+    def __repr__(self):
+        return f"flashcard({self.question}, {self.answer})"
+        
